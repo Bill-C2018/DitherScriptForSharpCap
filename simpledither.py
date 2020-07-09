a28788f55d870c7d4abbcf70750f0236a0189bb5
bill <wcunning81@gmail.com>
2020-07-09 17:15:10 -0400

    it works

--- simpledither.py
+++ simpledither.py
@@ -9,9 +9,20 @@ import urllib2
 class GlobalVariables:
 	message = []
 	doRun = True
+	ditherEvery = 5
+	ditherstring = ""
+	is_guiding = False
+	is_dithering = False;
 	
+class DitherVariables:
+	RAOnly = False
+	MaxPixels = 10
+	SettleTarget = 2
+	SettleDelay = 10
+	SettleMaximum = 60
 
-
+host = '127.0.0.1'
+port = 4400
 
 #///////////////////////////////////////////////////////////////////////
 #///////////////////////////////////////////////////////////////////////
@@ -44,6 +55,69 @@ def readMessage():
 	finally:
 		lock.release()
 	return themessage
+	
+#///////////////////////////
+def threaded_listen():
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		s.connect((host, port))
+	except:
+		print 'Unable to connect to phd server'
+		exit()
+
+	for l in s.makefile():
+		m = json.loads(l)
+		if 'Event' in m and m['Event'] != 'LoopingExposures':
+			eventtext = m['Event']
+			setMessage(buildMessage("PHD", eventtext))
+			if m['Event'] == 'StartGuiding':
+				GlobalVars.is_guiding = True
+			if m['Event'] == 'GuideStep' and GlobalVars.is_dithering != True:
+				GlobalVars.is_guiding = True
+			if m['Event'] == 'GuidingStopped':
+				GlobalVars.is_guiding = False
+			if m['Event'] == 'GuidingDithered':
+				GlobalVars.is_guiding = False
+			if m['Event'] == 'SettleDone':
+				GlobalVars.is_guiding = True
+			sleep(1)
+			if GlobalVars.doRun == False:
+				exit()
+
+def waitForGuiding():
+	while GlobalVars.is_guiding == False:
+		sleep(1)
+		
+
+def phdDither():
+	message = GlobalVars.ditherstring
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		s.connect((host, port))
+	except:
+		print 'Unable to connect to phd server'
+		exit()
+		
+	s.send(message)
+
+def buildDitherString():
+	message = '{"method": "dither", "params": ['
+	message = message + str(ditherVars.MaxPixels)
+	if(ditherVars.RAOnly == True):
+		message = message + ', true, { "pixels": '
+	else:
+		message = message + ', false,  { "pixels": '
+
+	message = message + str(ditherVars.SettleTarget)
+	message = message + ', "time": '
+	message = message + str(ditherVars.SettleDelay)
+	message = message + ', "timeout": '
+	message = message + str(ditherVars.SettleMaximum)
+	message = message + '}], "id":' + str(42) + '}\r\n'
+	GlobalVars.ditherstring = message
+	setMessage(buildMessage("DEBUG", GlobalVars.ditherstring))
+	
 #///////////////////////////
 def sharpCapGetImageCount():
 	return SharpCap.SelectedCamera.CapturedFrameCount
@@ -51,6 +125,20 @@ def sharpCapGetImageCount():
 def sharpCapIsCapturing():
 	return SharpCap.SelectedCamera.Capturing
 	
+#//////////////////////////////
+
+def doDither():
+	SharpCap.SelectedCamera.Paused = True
+	setMessage(buildMessage("STATUS", "Dithering"))
+	setMessage(buildMessage("STATUS", GlobalVars.ditherstring ))
+	GlobalVars.is_dithering = True
+	GlobalVars.is_guiding = False
+	phdDither()
+	GlobalVars.is_guiding = False
+	waitForGuiding()
+	GlobalVars.is_dithering = False
+	SharpCap.SelectedCamera.Paused = False
+
 #//////////////////////////////
 def statusLoop():
 
@@ -69,6 +157,9 @@ def statusLoop():
 #///////////////////////////
 def mainRunLoop():
 
+	localFrameCount = 0
+	lastHardFrameCount = 0
+	buildDitherString()
 	while GlobalVars.doRun == True:
 		sleep(1)
 		if sharpCapIsCapturing():
@@ -76,9 +167,20 @@ def mainRunLoop():
 			frameCount = sharpCapGetImageCount()
 			message = "Frame count " + str(frameCount)
 			setMessage(buildMessage("STATUS", message))
+			if lastHardFrameCount != frameCount:
+				lastHardFrameCount = frameCount
+				localFrameCount = localFrameCount + 1
+				if localFrameCount >= GlobalVars.ditherEvery:
+					doDither()
+					localFrameCount = 0
 		else:
 			setMessage(buildMessage("STATUS", "looping"))
+			localFrameCount = lastHardFrameCount = 0
 		
+
+def stopScript():
+	GlobalVars.doRun = False
+	
 
 #///////////////////////////////////////////////////////////////////////
 #///////////////////////////////////////////////////////////////////////
@@ -86,9 +188,14 @@ def mainRunLoop():
 #//////////////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////////
 GlobalVars = GlobalVariables
+ditherVars = DitherVariables
 lock = threading.Lock()
 
+
+SharpCap.AddCustomButton("Stop", None, "StopDitheredRun", stopScript)
 tRun = threading.Thread(target=mainRunLoop, args=[])
 tStatus = threading.Thread(target=statusLoop, args=[])
+tPhd = threading.Thread(target=threaded_listen, args=[])
 tRun.start()
 tStatus.start()
+tPhd.start()
