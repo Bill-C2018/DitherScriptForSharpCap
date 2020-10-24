4a1642ce9c43e3ea3f021ab4430b912311c40320
bill-c2018 <wcunning81@gmail.com>
2020-10-24 12:53:28 -0700

    complete and cleanup

--- client.py
+++ client.py
@@ -2,12 +2,32 @@ import socket
 import sys
 import json
 
+HOST_ADDRESS = "localhost"
+HOST_PORT = 5322
+TIMEOUT = 5.0
+
+'''
+=============================================================================
+do not edit anything below here
+=============================================================================
+'''
 
 endOfLine = "\r\n"
 valid_cmds = ["ditherevery","waitfornextframe","raonly", "settletarget", "settledelay", "settlemax", "maxpixels", "ditherstring"]
 valid_ops = ["get", "set"]
+data = None
 
-def isValidCmd(cmd):
+def print_valid_cmds():
+	for c in valid_cmds:
+		msg = c + "\r\n"
+		print (msg)
+		
+def print_valid_ops():
+	for o in valid_ops:
+		msg = c + "\r\n"
+		print (msg)
+		
+def is_valid_cmd(cmd):
 
 	for x in valid_cmds:
 		if x == cmd:
@@ -15,7 +35,7 @@ def isValidCmd(cmd):
 		
 	return False
 
-def isValidOp(op):	
+def is_valid_op(op):	
 
 	for x in valid_ops:
 		if x == op:
@@ -23,54 +43,75 @@ def isValidOp(op):
 		
 	return False
 	
-print(str(sys.argv))
-
-if isValidOp(sys.argv[1]) == False:
-	print("Invalid operation")
-	sys.exit()
+def validate_cmd_line():
+	print(str(sys.argv))
 	
-if isValidCmd(sys.argv[2]) == False:
-	print("invalid command")
-	sys.exit()
+	if is_valid_op(sys.argv[1]) == False:
+		print("Invalid operation")
+		print_valid_ops()
+		sys.exit()
+		
+	if is_valid_cmd(sys.argv[2]) == False:
+		print("invalid command")
+		print_valid_cmds()
+		sys.exit()
+	
+	if sys.argv[1] == "get":
+		if not sys.argv[3]:
+			data = "0"
+		else:
+			data = sys.argv[3]
+	else:
+		if not sys.argv[3]:
+			print("Value is required for set operation")
+			sys.exit()
 
+
+def connect_and_transmit(cmd_block):
+	# Create a TCP/IP socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	# Connect the socket to the port where the server is listening
+	server_address = (HOST_ADDRESS, HOST_PORT)
+	print(server_address)
+	sock.settimeout(TIMEOUT)
+	sock.connect(server_address)
+	
+	
+	try:
+	    
+	    # Send data
+	    message = json.dumps(cmd_block)
+	    print( message)
+	    sock.sendall(bytes(message,'ascii'))
+
+	    # Recieve response
+	    eolr = False
+	    while eolr == False:
+	        data = sock.recv(255)
+	        sdata = str(data,'ascii')
+	        print( sdata)
+	        if endOfLine in sdata:
+	            eolr = True
+
+	finally:
+	    print("closing socket")
+	    sock.close()
+	    
+'''
+===============================================================================
+Main functionality
+===============================================================================
+'''
+			
+validate_cmd_line()
+		
 cmd_block = {
     "op" : sys.argv[1],
     "cmd" : sys.argv[2],
-    "value" : sys.argv[3]
+    "value" : data
 }
 
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 5322)
-print(server_address)
-sock.settimeout(5.0)
-sock.connect(server_address)
+connect_and_transmit(cmd_block)
 
 
-try:
-    
-    # Send data
-    message = json.dumps(cmd_block)
-    print( message)
-    sock.sendall(bytes(message,'ascii'))
-
-    # Look for the response
-    amount_received = 0
-    
-#    while amount_received < 5:
-    eolr = False
-    while eolr == False:
-        data = sock.recv(255)
-        sdata = str(data,'ascii')
-        print( sdata)
-        if endOfLine in sdata:
-            eolr = True
-
-#break
-
-finally:
-    print("closing socket")
-    sock.close()
